6f4b1d504b04999c1904e0813b4f79be5108a7d0
felsager <felsager@users.noreply.github.com>
2023-07-26 00:02:20 +0200

    Fixed bug which gave wrong no. of wins. Faster

--- frogger_hard/frogger_hard.cpp
+++ frogger_hard/frogger_hard.cpp
@@ -5,17 +5,18 @@
 #include <bitset>
 #include <unordered_set>
 #include <algorithm>
-#include <execution>
+#include <immintrin.h>
 
 #include <cstdint>
 #include <cstdlib>
 #include <ctime>
 
 #pragma GCC optimize("Ofast,inline") // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
-//#pragma GCC optimize ("unroll-loops")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("avx")
 
 constexpr const int N_BITSET = 4e5;
-constexpr const int N_MAX = 4e5;
+constexpr const int N_MAX = 2e5;
 constexpr int unroll_factor = 4;
 
 using namespace std;
@@ -45,71 +46,80 @@ private:
     uint64_t blocks[BLOCK_COUNT];
 };
 
+void updatePreWinsWithVisitedTiles(int pre_wins[], const int visited_tiles[], int num_full_blocks) {
+    // The unroll factor should be a multiple of 4 for better vectorization
+    int j = 0;
+
+    // Process full blocks of 4 elements each
+    for (; j < num_full_blocks; j += unroll_factor) {
+        // Load 4 elements of visited_tiles into SIMD registers
+        __m128i visited_vec = _mm_loadu_si128(reinterpret_cast<const __m128i*>(&visited_tiles[j*unroll_factor]));
+
+        // Load 4 elements of pre_wins into SIMD registers
+        __m128i pre_wins_vec = _mm_loadu_si128(reinterpret_cast<const __m128i*>(&pre_wins[j*unroll_factor]));
+
+        // Perform the addition between visited_vec and pre_wins_vec
+        pre_wins_vec = _mm_add_epi32(pre_wins_vec, visited_vec);
+
+        // Store the result back to pre_wins array
+        _mm_storeu_si128(reinterpret_cast<__m128i*>(&pre_wins[j*unroll_factor]), pre_wins_vec);
+    }
+
+    // Process the remaining elements (less than 4) if any
+    for (; j < N_MAX; j++) {
+        pre_wins[j] += visited_tiles[j];
+    }
+}
+
 int winning_instances(int board[], const int n) {
     int no_wins = 0; // number of winning instances
-    int num_full_blocks = n - (n % 4);
+    int num_full_blocks = n / unroll_factor;
     int board_val;
     bool cycle;
     bool outside;
     unordered_set<int> magic_numbers(board, board + N_MAX); // unique magic numbers
-    short pre_wins[N_MAX]; // tiles where the no. wins from that tile is noted
-    CustomBitset visited_tiles; 
+    int pre_wins[N_MAX]; // tiles where the no. wins from that tile is noted
+    int visited_tiles[N_MAX]; 
     CustomBitset used_magic_numbers;
     int idxs[N_MAX]; // keeps track of the next move
     for (int i = 0; i < n; i++) {
         idxs[i] = board[i] + i;
         pre_wins[i] = 0;
     }
-    //fill(pre_wins.begin(), pre_wins.end(), 0);
-    //copy(board.begin(), board.end(), inserter(magic_numbers, magic_numbers.end()));
     for (int i = 0; i < n; i++) {
-        visited_tiles.reset();
         used_magic_numbers.reset();
         cycle = false;
         outside = false;
-        int idx = i;
+        int idx = i;  
+        for (int i = 0; i < n; i++) {
+            visited_tiles[i] = 0;
+        }
         while (!cycle && !outside) {
+            board_val = board[idx];
+            visited_tiles[idx] = 1;
             if (pre_wins[idx] != 0) {
-                no_wins += pre_wins[idx];
                 break;
             }
-            visited_tiles.set(idx);
-            board_val = board[idx];
-            idx = idxs[idx];
-            auto magic_num_it = magic_numbers.find(board_val);
-            if (magic_num_it != magic_numbers.end() && !used_magic_numbers.test(board_val + n)) {
-                no_wins += 1;
-                used_magic_numbers.set(board_val + n);
-
-                for (int j = 0; j < num_full_blocks; j += unroll_factor) {
-                    pre_wins[j] += visited_tiles.test(j);
-                    pre_wins[j + 1] += visited_tiles.test(j + 1);
-                    pre_wins[j + 2] += visited_tiles.test(j + 2);
-                    pre_wins[j + 3] += visited_tiles.test(j + 3);
-                }
-
-                for (int j = num_full_blocks; j < n; j++) {
-                    pre_wins[j] += visited_tiles.test(j);
+            else if (!used_magic_numbers.test(board_val + n)) {
+                auto magic_num_it = magic_numbers.find(board_val);
+                if (magic_num_it != magic_numbers.end()) {
+                    updatePreWinsWithVisitedTiles(pre_wins, visited_tiles, num_full_blocks);
+                    used_magic_numbers.set(board_val + n);
                 }
             }
+            idx = idxs[idx];
             if (idx < 0 || idx >= n) {
                 outside = true;
             }
-            else if (visited_tiles.test(idx)) {
+            else if (visited_tiles[idx] == 1) {
                 cycle = true;
-                for (int j = 0; j < num_full_blocks; j += unroll_factor) {
-                    pre_wins[j] += visited_tiles.test(j);
-                    pre_wins[j + 1] += visited_tiles.test(j + 1);
-                    pre_wins[j + 2] += visited_tiles.test(j + 2);
-                    pre_wins[j + 3] += visited_tiles.test(j + 3);
-                }
-                
-                for (int j = num_full_blocks; j < n; j++) {
-                    pre_wins[j] += visited_tiles.test(j);
-                }           
+                updatePreWinsWithVisitedTiles(pre_wins, visited_tiles, num_full_blocks);
             }
         };
     } 
+    for (int i = 0; i < n; i++) {
+        no_wins += pre_wins[i];
+    }
     return no_wins;
 }
 
@@ -139,8 +149,8 @@ int main() {
             val = rand() % (2 * n) - n;
         } while (val == 0);
         board[i] = val;
+        //cout << val << " ";
     }
-
     // Call the winning_instances function and measure the time taken
     clock_t startTime = clock();
     int no_wins = winning_instances(board, n);
