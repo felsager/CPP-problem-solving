1bed1cc5e352c041854ece404705a375e4ab63cc
felsager <felsager@users.noreply.github.com>
2023-07-26 21:52:56 +0200

    error in algorithm

--- frogger_hard/frogger_hard_refactor.cpp
+++ frogger_hard/frogger_hard_refactor.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <vector>
-#include <bitset>
 #include <unordered_set>
 #include <algorithm>
+#include <numeric>
 #include <immintrin.h>
 
-#include <cstdint>
-#include <cstdlib>
-#include <ctime>
-
+// compiler flags
 #pragma GCC optimize("Ofast,inline") // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
 #pragma GCC optimize ("unroll-loops")
 #pragma GCC target("avx")
 
-constexpr const int N_BITSET = 4e5;
 constexpr const int N_MAX = 2e5;
 constexpr int unroll_factor = 4;
 
@@ -23,6 +18,7 @@ using namespace std;
 
 class CustomBitset {
 public:
+    static const int N_BITSET = 4e5;
     static const int BITS_PER_BLOCK = 64;
     static const int BLOCK_COUNT = (N_BITSET + BITS_PER_BLOCK - 1) / BITS_PER_BLOCK;
 
@@ -60,64 +56,53 @@ void updatePreWins(int pre_wins[], const int visited_tiles[], int num_full_block
 }
 
 int winning_instances(int board[], const int n) {
-    int no_wins = 0; // number of winning instances
-    int num_full_blocks = n / unroll_factor;
+    int idx;
+    int pre_idx;
     int board_val;
-    unordered_set<int> magic_numbers(board, board + N_MAX); // unique magic numbers
+    CustomBitset magic_numbers;
     int pre_wins[N_MAX]; // tiles where the no. wins from that tile is noted
-    int visited_tiles[N_MAX]; 
-    CustomBitset used_magic_numbers;
-    int idxs[N_MAX]; // keeps track of the next move
+    int visited_tiles[N_MAX];
+    int idxs[N_MAX];
+    int num_full_blocks = n / unroll_factor;
     for (int i = 0; i < n; i++) {
         idxs[i] = board[i] + i;
         pre_wins[i] = 0;
     }
+
     for (int i = 0; i < n; i++) {
-        used_magic_numbers.reset();  
-        for (int j = 0; i < n; i++) {
-            visited_tiles[i] = 0;
+        idx = i;
+        if (pre_wins[idx] != 0) {
+            continue;
         }
-        int idx = i;
+        magic_numbers.reset();
+        fill(visited_tiles, visited_tiles + n, 0);
         for (int j = 0; j < n; j++) {
             visited_tiles[idx] = 1;
-            idx = idxs[idx];
             board_val = board[idx];
-            cout << "\n";
-            for (int j = 0; j < n; j++) {
-                cout << "visited_tiles[" << j << "] = " << visited_tiles[j] << " ";
-            }
-            cout << "\n";
-            if (pre_wins[idx] != 0) {
-                break;
-            }
-            else if (!used_magic_numbers.test(board_val + n)) {
-                auto magic_num_it = magic_numbers.find(board_val);
-                if (magic_num_it != magic_numbers.end()) {
-                    updatePreWins(pre_wins, visited_tiles, num_full_blocks);
-                    used_magic_numbers.set(board_val + n);
-                }
-            }
-            if (idx < 0 || idx >= n) {
-                break;
-            }
-            else if (visited_tiles[idx] == 1) {
-                visited_tiles[idx] = 0;
+            cout << "test = " << !magic_numbers.test(board_val + n) << "\n\n";
+            if (!magic_numbers.test(board_val + n)) {
+                magic_numbers.set(board_val + n);
                 updatePreWins(pre_wins, visited_tiles, num_full_blocks);
+            }
+            pre_idx = idx;
+            idx = idxs[idx];
+            if (visited_tiles[idx]) {
+                pre_wins[pre_idx] += 1;
+                break;
+            }
+            else if (idx < 0 || idx >= n) {
                 break;
             }
         }
-    } 
-    for (int i = 0; i < n; i++) {
-        no_wins += pre_wins[i];
     }
+    int no_wins = accumulate(pre_wins, pre_wins + n, 0);
     return no_wins;
 }
 
 int main() {
+    int board[N_MAX];
     /*int n;
     cin >> n;
-
-    vector<int> board(n); 
     string input_line;
     getline(cin >> ws, input_line);
     stringstream ss(input_line);
@@ -130,8 +115,7 @@ int main() {
     cout << "Enter the size of the board (n): ";
     cin >> n;
 
-    //vector<int> board(n);
-    int board[N_MAX];
+    
     // Generate a random board with values between 0 and n-1
     for (int i = 0; i < n; i++) {
         int val;
@@ -139,7 +123,7 @@ int main() {
             val = rand() % (2 * n) - n;
         } while (val == 0);
         board[i] = val;
-        //cout << val << " ";
+        cout << val << " ";
     }
     // Call the winning_instances function and measure the time taken
     clock_t startTime = clock();
